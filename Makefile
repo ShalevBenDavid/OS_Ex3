06c5537cdf79689b91c5fa28c46713a145c44131
ron_shuster <118674717+shusteron@users.noreply.github.com>
2023-05-09 00:08:52 +0300

    Commit #5 - Starting client preformance

--- Makefile
+++ Makefile
@@ -2,11 +2,12 @@
 
 CC = gcc
 FLAGS = -Wall -g
+LDFLAGS = -lssl -lcrypto
 
 all: stnc
 
 stnc: stnc.c
-	$(CC) $(FLAGS) stnc.c -o stnc
+	$(CC) $(FLAGS) stnc.c -o stnc $(LDFLAGS)
 
 clean:
 	rm -f *.o stnc
--- stnc.c
+++ stnc.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-//#include <openssl/md5.h>
+#include <openssl/md5.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -17,39 +17,59 @@
 #define BUFFER_SIZE 104857600 // 100 MB.
 #define MAX_CONNECTIONS 1 // Allowing only one connection.
 #define NUM_OF_FD 2 // Number of file descriptors we monitor in poll().
+#define CHECKSUM_SIZE 16 // The size of the checksum header.
 
 // Methods.
 void handle_client (int, char*[]);
 void handle_server (int, char*[]);
-void handle_client_performance (int, char*[]);
-void handle_server_performance (int, char*[]);
+void handle_client_performance (int, char*[], bool[], bool[], char*);
+void handle_server_performance (int, char*[], bool);
 void md5_checksum (unsigned char*, int, unsigned char*);
-void generate100MB (char*);
+unsigned char* generateData (int size);
+bool check_checksums (unsigned char*, unsigned char*);
+void print_server_usage ();
+void print_client_usage ();
+
+
 
 int main(int argc, char* argv[]){
     // Define flags to indicate execute usage.
     bool p_flag = false;
     bool q_flag = false;
-    bool ipv4_flag = false;
-    bool ipv6_flag = false;
-    bool tcp_flag = false;
-    bool udp_flag = false;
+    bool types[5] = {false};
+    bool params[4] = {false};
     bool is_client = !strcmp(argv[1], "-c");
     bool is_server = !strcmp(argv[1], "-s");
+    char* filename;
+
     for (int i = 0; i < argc; i++) {
         if (!strcmp(argv[i], "-p")) { p_flag = true; }
         if (!strcmp(argv[i], "-q")) { q_flag = true; }
-        if (!strcmp(argv[i], "ipv4")) {ipv4_flag = true; }
-        if (!strcmp(argv[i], "ipv6")) {ipv6_flag = true; }
-        if (!strcmp(argv[i], "tcp")) {tcp_flag = true; }
-        if (!strcmp(argv[i], "udp")) {udp_flag = true; }
+        if (!strcmp(argv[i], "ipv4")) { types[0] = true; }
+        if (!strcmp(argv[i], "ipv6")) { types[1] = true; }
+        if (!strcmp(argv[i], "mmap")) { types[2] = true; }
+        if (!strcmp(argv[i], "pipe")) { types[3] = true; }
+        if (!strcmp(argv[i], "uds")) { types[4] = true; }
+        if (!strcmp(argv[i], "udp")) { params[0] = true; }
+        if (!strcmp(argv[i], "tcp")) { params[1] = true; }
+        if (!strcmp(argv[i], "dgram")) { params[2] = true; }
+        if (!strcmp(argv[i], "stream")) { params[3] = true; }
+    }
+    if( (types[2] || types[3])) {
+        if (argc != 7 ) {
+            print_server_usage();
+            print_client_usage();
+            exit(EXIT_FAILURE);
+        } else {
+            filename = argv[6];
+        } 
     }
     if (is_client) {
-        if (p_flag) { handle_client_performance(argc, argv); }
+        if (p_flag) { handle_client_performance(argc, argv, types, params, filename); }
         else { handle_client(argc, argv);}
     }
     else if (is_server) {
-        if (p_flag) { handle_server_performance(argc, argv); }
+        if (p_flag) { handle_server_performance(argc, argv, q_flag); }
         else { handle_server(argc, argv); }
     }
     else {
@@ -61,8 +81,8 @@ int main(int argc, char* argv[]){
 //---------------------------------- Client Side ---------------------------------
 void handle_client (int argc, char* argv[]) {
     // Not the right format.
-    if(argc < 4){
-        printf("(-) The usage is \"stnc -c IP PORT -p (p for performance test) <type> <param>\"\n");
+    if(argc != 4){
+        print_client_usage();
         exit(EXIT_FAILURE);
     }
 
@@ -115,7 +135,8 @@ void handle_client (int argc, char* argv[]) {
     }
 
     // Create pollfd to check which fd have current events.
-    struct pollfd *pfds = malloc(sizeof *pfds * NUM_OF_FD);
+    struct pollfd *pfds = calloc(NUM_OF_FD, sizeof(struct pollfd));
+
     if (!pfds) {
         printf("(-) Failed to allocate memory for poll. %d\n" , errno);
         exit(EXIT_FAILURE);
@@ -142,7 +163,7 @@ void handle_client (int argc, char* argv[]) {
             fgets(buffer, SIZE + 1, stdin);
             // Send message to the client.
             int sbytes = send(socketFD, buffer, strlen(buffer), 0);
-            if(sbytes < 0){
+            if(sbytes == -1){
                 printf("(-) send() failed with error code: %d\n", errno);
             }
             bzero(buffer,SIZE);
@@ -167,8 +188,8 @@ void handle_client (int argc, char* argv[]) {
 //---------------------------------- Server Side ---------------------------------
 void handle_server (int argc, char* argv[]) {
 // Not the right format.
-    if (argc < 3) {
-        printf("(-) The usage is \"stnc -s port -p (p for performance test) -q (q for quiet)\"\n");
+    if (argc != 3) {
+        print_server_usage();
         exit(EXIT_FAILURE);
     }
 
@@ -229,7 +250,7 @@ void handle_server (int argc, char* argv[]) {
         }
 
         // Create polled to check which fd have current events.
-        struct pollfd *pfds = malloc(sizeof *pfds * NUM_OF_FD);
+        struct pollfd *pfds = calloc(NUM_OF_FD, sizeof(struct pollfd));
         if (!pfds) {
             printf("(-) Failed to allocate memory for poll. %d\n", errno);
             exit(EXIT_FAILURE);
@@ -256,7 +277,7 @@ void handle_server (int argc, char* argv[]) {
                 fgets(buffer, SIZE + 1, stdin);
                 // Send message to the client.
                 int sbytes = send(clientSocket, buffer, strlen(buffer), 0);
-                if (sbytes < 0) {
+                if (sbytes == -1) {
                     printf("(-) send() failed with error code: %d\n", errno);
                 }
             }
@@ -280,20 +301,21 @@ void handle_server (int argc, char* argv[]) {
 }
 
 //---------------------------------- Client Side - Performance Mode ---------------------------------
-void handle_client_performance (int argc, char* argv[]) {
+void handle_client_performance (int argc, char* argv[], bool types[], bool params[], char* filename) {
     // Not the right format.
     if(argc < 4){
-        printf("(-) The usage is \"stnc -c IP PORT -p (p for performance test) <type> <param>\"\n");
+        print_client_usage();
         exit(EXIT_FAILURE);
     }
 
-    // Initialize variables for client or server appropriately.
-    char buffer[SIZE + 1] = {0}; // size + 1 for the \0.
+    // Generate 100MB of data.
+    unsigned char* buffer = generateData(BUFFER_SIZE);
+
+    // Initialize variables for server.
     struct sockaddr_in serverAddress;
 
-    // Initialize port and address.
+    // Initialize port.
     int port = atoi(argv[3]);
-    char* address = argv[2];
 
     // Resting address.
     memset(&serverAddress, 0, sizeof(serverAddress));
@@ -301,17 +323,13 @@ void handle_client_performance (int argc, char* argv[]) {
     serverAddress.sin_family = AF_INET;
     // Setting the port.
     serverAddress.sin_port = htons(port);
-    // Setting the IP address,
-    serverAddress.sin_addr.s_addr = inet_addr(address);
-    // If converting address files, exit.
-    if (inet_pton(AF_INET, address, &serverAddress.sin_addr) == 0) {
-        printf("(-) Failed to convert IPv4 address to binary! -> inet_pton() failed with error code: %d\n", errno);
-        exit(EXIT_FAILURE);
-    }
+    // Allow everyone to connect.
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    
 
-    // <<<<<<<<<<<<<<<<<<<<<<<<< Create TCP Connection >>>>>>>>>>>>>>>>>>>>>>>>>
-    // Creates TCP socket.
-    int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    // <<<<<<<<<<<<<<<<<<<<<<<<< Create UDP Connection To Transmit <type> And <param> >>>>>>>>>>>>>>>>>>>>>>>>>
+    // Creates UDP socket.
+    int socketFD = socket(AF_INET, SOCK_DGRAM, 0);
 
     // Check if we were successful in creating socket.
     if (socketFD == -1) {
@@ -319,77 +337,24 @@ void handle_client_performance (int argc, char* argv[]) {
         exit(EXIT_FAILURE); // Exit program and return EXIT_FAILURE (defined as 1 in stdlib.h).
     }
     else {
-        printf("(=) Socket created successfully.\n");
+        printf("(=) UDP Socket created successfully.\n");
     }
-
-    // Create connection with server.
-    int connection = connect(socketFD, (struct sockaddr*) &serverAddress, sizeof(serverAddress));
-
-    // Check if we were successful in connecting with server.
-    if (connection == -1) {
-        printf("(-) Could not connect to server! -> connect() failed with error code: %d\n", errno);
-        close(socketFD);
+    // Sending the <type> and <param>.
+    int sbytes1 = sendto(socketFD, argv[5], strlen(argv[5]), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    int sbytes2 = sendto(socketFD, argv[6], strlen(argv[6]), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    if (sbytes1 || sbytes2 == -1) {
+        printf("(-) sendto() failed with error code: %d\n", errno);
         exit(EXIT_FAILURE);
     }
-    else {
-        printf("(=) Connection with server established.\n\n");
-    }
-
-    // Create pollfd to check which fd have current events.
-    struct pollfd *pfds = malloc(sizeof *pfds * NUM_OF_FD);
-    if (!pfds) {
-        printf("(-) Failed to allocate memory for poll. %d\n" , errno);
-        exit(EXIT_FAILURE);
-    }
-
-    pfds[0].fd = STDIN_FILENO;
-    pfds[0].events = POLLIN;
-    pfds[1].fd = socketFD;
-    pfds[1].events = POLLIN;
-
-    while (true) {
-        // Call poll and see how events occurred.
-        int poll_count = poll(pfds, NUM_OF_FD, -1);
-        if (poll_count == -1) {
-            printf("(-) poll() failed with error code: %d\n", errno);
-            exit(EXIT_FAILURE);
-        }
-
-        // <<<<<<<<<<<<<<<<<<<<<<<<< Send To Socket >>>>>>>>>>>>>>>>>>>>>>>>>
-        if (pfds[0].revents & POLLIN) {
-            // Reset buffer.
-            bzero(buffer, SIZE);
-            // Get message from standard input.
-            fgets(buffer, SIZE + 1, stdin);
-            // Send message to the client.
-            int sbytes = send(socketFD, buffer, strlen(buffer), 0);
-            if(sbytes < 0){
-                printf("(-) send() failed with error code: %d\n", errno);
-            }
-            bzero(buffer,SIZE);
-        }
-            // <<<<<<<<<<<<<<<<<<<<<<<<< Read From Socket >>>>>>>>>>>>>>>>>>>>>>>>>
-        else if (pfds[1].revents & POLLIN) {
-            // Reset buffer.
-            bzero(buffer, SIZE);
-            // Receive message on the socket.
-            int nbytes = recv(socketFD, buffer, SIZE, 0);
-            if (nbytes == 0) {
-                printf("(-) Server hung up.\n");
-            } else if (nbytes == -1) {
-                printf("(-) recv() failed with error code: %d\n", errno);
-            }
-            // Print client message.
-            printf("Message: %s", buffer);
-        }
-    }
+    
+    
 }
 
 //---------------------------------- Server Side - Performance Mode ---------------------------------
-void handle_server_performance (int argc, char* argv[]) {
+void handle_server_performance (int argc, char* argv[], bool q_flag) {
 // Not the right format.
     if (argc < 3) {
-        printf("(-) The usage is \"stnc -s port -p (p for performance test) -q (q for quiet)\"\n");
+        print_server_usage();
         exit(EXIT_FAILURE);
     }
 
@@ -501,18 +466,61 @@ void handle_server_performance (int argc, char* argv[]) {
 }
 
 //---------------------------------- Checksum Function ---------------------------------
-//void md5_checksum(unsigned char *data, int len, unsigned char *checksum) {
-//    MD5_CTX md5;
-//
-//    MD5_Init(&md5);
-//    MD5_Update(&md5, data, len);
-//    MD5_Final(checksum, &md5);
-//}
+void md5_checksum (unsigned char* data, int size, unsigned char* checksum) {
+   // Initializing MD5 struct. 
+   MD5_CTX md5;
 
-//---------------------------------- Checksum 2MB Of Data ---------------------------------
-void generate100MB (char* buffer) {
-    for (int i = 0; i < BUFFER_SIZE; i++) {
-        buffer[i] = rand() % 256;
-    }
+   MD5_Init(&md5);
+   MD5_Update(&md5, data, size);
+   MD5_Final(checksum, &md5);
 }
+
+//---------------------------------- Generate Data And Add Checksum ---------------------------------
+unsigned char* generateData (int size) {
+    unsigned char* result = NULL;
+
+    // User wants to generate nothing. 
+    if(size == 0) {
+        return NULL;
+    }
+
+    result = (unsigned char*)malloc((size + CHECKSUM_SIZE) * sizeof(char));
+
+    // iI malloc failed return NULL.
+    if(!result) {
+        return NULL;
+    }
+
+    // Randomaize data.
+    srand(time(NULL));
+    for (int i = 0; i < size; i++) {
+        result[i] = ((char)rand() % 256);
+    }
+
+    // Call checksum.
+    md5_checksum(result, size, result + size);
+    
+    // Returning the pointer to the data.
+    return result;
+}
+
+//---------------------------------- Compare Checksums ---------------------------------
+bool check_checksums(unsigned char* checksum1, unsigned char* checksum2) {
+    for (int i = 0; i < CHECKSUM_SIZE; i++) {
+        if (checksum1[i] != checksum2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+//---------------------------------- Print Usages ---------------------------------------
+void print_server_usage() {
+    printf("(-) The usage is \"stnc -s port -p (p for performance test) -q (q for quiet)\"\n");
+}
+void print_client_usage() {
+    printf("(-) The usage is \"stnc -c IP PORT -p (p for performance test) <type> <param>\"\n");
+}
+
+
 
