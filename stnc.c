25f7b1aed3d629b0c4364a3c703315cb0c4fc91e
Shalev Ben David <105733245+ShalevBenDavid@users.noreply.github.com>
2023-05-11 16:18:35 +0300

    Commit #7 - Changing to ssize + fixing recv function

--- stnc.c
+++ stnc.c
@@ -19,23 +19,21 @@
 #define MAX_CONNECTIONS 1 // Allowing only one connection.
 #define NUM_OF_FD 2 // Number of file descriptors we monitor in poll().
 #define CHECKSUM_SIZE 16 // The size of the checksum header.
+#define PARAM_LEN 6 // Max parameter length.
+#define TYPE_LEN 4 // Max type length.
 
 // Methods.
 void handle_client (int, char*[]);
 void handle_server (int, char*[]);
-void handle_client_performance (int, char*[], bool[], bool[], char*);
+void handle_client_performance (int, char*[], const bool[], const bool[], char*);
 void handle_server_performance (int, char*[], bool);
 void md5_checksum (unsigned char*, int, unsigned char*);
 unsigned char* generateData (int size);
-bool check_checksums (unsigned char*, unsigned char*);
+void check_checksums (const unsigned char*, const unsigned char*);
 void print_server_usage ();
 void print_client_usage ();
-size_t send_data(unsigned char[], int);
-int recv_data(int );
-
-
-
-
+size_t send_data (unsigned char[], int);
+void recv_data (int, char*, char*, unsigned char*);
 
 int main(int argc, char* argv[]){
     // Define flags to indicate execute usage.
@@ -81,6 +79,9 @@ int main(int argc, char* argv[]){
         printf("(-) Not the right format.\n");
         exit(EXIT_FAILURE);
     }
+
+    printf("(=) Exiting...\n");
+    return EXIT_SUCCESS;
 }
 
 //---------------------------------- Client Side ---------------------------------
@@ -167,7 +168,7 @@ void handle_client (int argc, char* argv[]) {
             // Get message from standard input.
             fgets(buffer, SIZE + 1, stdin);
             // Send message to the client.
-            int sbytes = send(socketFD, buffer, strlen(buffer), 0);
+            ssize_t sbytes = send(socketFD, buffer, strlen(buffer), 0);
             if(sbytes == -1){
                 printf("(-) send() failed with error code: %d\n", errno);
             }
@@ -178,7 +179,7 @@ void handle_client (int argc, char* argv[]) {
             // Reset buffer.
             bzero(buffer, SIZE);
             // Receive message on the socket.
-            int nbytes = recv(socketFD, buffer, SIZE, 0);
+            ssize_t nbytes = recv(socketFD, buffer, SIZE, 0);
             if (nbytes == 0) {
                 printf("(-) Server hung up.\n");
             } else if (nbytes == -1) {
@@ -281,7 +282,7 @@ void handle_server (int argc, char* argv[]) {
                 // Get message from standard input.
                 fgets(buffer, SIZE + 1, stdin);
                 // Send message to the client.
-                int sbytes = send(clientSocket, buffer, strlen(buffer), 0);
+                ssize_t sbytes = send(clientSocket, buffer, strlen(buffer), 0);
                 if (sbytes == -1) {
                     printf("(-) send() failed with error code: %d\n", errno);
                 }
@@ -291,7 +292,7 @@ void handle_server (int argc, char* argv[]) {
                 // Reset buffer.
                 bzero(buffer, SIZE);
                 // Receive message on the socket.
-                int nbytes = recv(clientSocket, buffer, SIZE, 0);
+                ssize_t nbytes = recv(clientSocket, buffer, SIZE, 0);
                 if (nbytes == 0) {
                     printf("(-) Client hung up\n");
                     break;
@@ -306,9 +307,9 @@ void handle_server (int argc, char* argv[]) {
 }
 
 //---------------------------------- Client Side - Performance Mode ---------------------------------
-void handle_client_performance (int argc, char* argv[], bool types[], bool params[], char* filename) {
+void handle_client_performance (int argc, char* argv[], const bool types[], const bool params[], char* filename) {
     // Not the right format.
-    if(argc < 4){
+    if(argc > 7){
         print_client_usage();
         exit(EXIT_FAILURE);
     }
@@ -325,7 +326,6 @@ void handle_client_performance (int argc, char* argv[], bool types[], bool param
     // Initialize port.
     int port = atoi(argv[3]);
     char* address = argv[2];
-    
 
     // Resting address.
     memset(&serverAddress, 0, sizeof(serverAddress));
@@ -349,107 +349,90 @@ void handle_client_performance (int argc, char* argv[], bool types[], bool param
         printf("(=) UDP Socket created successfully.\n");
     }
     // Sending the <type> and <param>.
-    int sbytes1 = sendto(socketFD, argv[5], strlen(argv[5]), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    ssize_t sbytes1 = sendto(socketFD, argv[5], strlen(argv[5]), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
     sleep(1);
-    int sbytes2 = sendto(socketFD, argv[6], strlen(argv[6]), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    ssize_t sbytes2 = sendto(socketFD, argv[6], strlen(argv[6]), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
     if (sbytes1 == -1 || sbytes2 == -1) {
         printf("(-) sendto() failed with error code: %d\n", errno);
         exit(EXIT_FAILURE);
     }
     printf("(+) Sent connection type successfully.\n");
 
-    // <<<<<<<<<<<<<<<<<<<<<<<<< Handeling All Combinations >>>>>>>>>>>>>>>>>>>>>>>>>
+    // <<<<<<<<<<<<<<<<<<<<<<<<< Handling All Combinations >>>>>>>>>>>>>>>>>>>>>>>>>
     sleep(1);
-    //  IPv4 && TCP
-    if(types[0] && params[0]){
-        //-------------------------------Create TCP Connection-----------------------------
-        int sock = socket(AF_INET, SOCK_STREAM, 0);
+    // TCP
+    if (params[0]) {
+        // IPv4
+        if (types[0]) {
+            //-------------------------------Create TCP Connection-----------------------------
+            int sock = socket(AF_INET, SOCK_STREAM, 0);
 
-        // Check if we were successful in creating socket.
-        if(sock == -1) {
-            printf("(-) Could not create socket! -> socket() failed with error code: %d\n", errno);
-            exit(EXIT_FAILURE); 
-        }
-        else {
-            printf("(=) Socket created successfully.\n");
-        }
+            // Check if we were successful in creating socket.
+            if (sock == -1) {
+                printf("(-) Could not create socket! -> socket() failed with error code: %d\n", errno);
+                exit(EXIT_FAILURE);
+            } else {
+                printf("(=) Socket created successfully.\n");
+            }
 
-        // Clean the server address we already created earlier.
-        memset(&serverAddress, '\0', sizeof(serverAddress));
+            // Clean the server address we already created earlier.
+            memset(&serverAddress, '\0', sizeof(serverAddress));
 
-        // Assign port and address to "serverAddress".
-        serverAddress.sin_family = AF_INET;
-        serverAddress.sin_port = htons(port); // Short, network byte order.
-        serverAddress.sin_addr.s_addr = inet_addr(address);
+            // Assign port and address to "serverAddress".
+            serverAddress.sin_family = AF_INET;
+            serverAddress.sin_port = htons(port); // Short, network byte order.
+            serverAddress.sin_addr.s_addr = inet_addr(address);
 
-        // Convert address to binary.
-        if (inet_pton(AF_INET, address, &serverAddress.sin_addr) <= 0)
-        {
-            printf("(-) Failed to convert IPv4 address to binary! -> inet_pton() failed with error code: %d\n", errno);
-            exit(EXIT_FAILURE);
-        }
+            // Convert address to binary.
+            if (inet_pton(AF_INET, address, &serverAddress.sin_addr) <= 0) {
+                printf("(-) Failed to convert IPv4 address to binary! -> inet_pton() failed with error code: %d\n",
+                       errno);
+                exit(EXIT_FAILURE);
+            }
 
-        //Create connection with server.
-        int connection = connect(sock, (struct sockaddr*) &serverAddress, sizeof(serverAddress));
+            //Create connection with server.
+            int connection = connect(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
 
-        // Check if we were successful in connecting with server.
-        if(connection == -1) {
-            printf("(-) Could not connect to server! -> connect() failed with error code: %d\n", errno);
-            exit(EXIT_FAILURE); // Exit program and return// EXIT_FAILURE (defined as 1 in stdlib.h).
-        }
-        else {
-            printf("(=) Connection with server established.\n\n");
-        }
+            // Check if we were successful in connecting with server.
+            if (connection == -1) {
+                printf("(-) Could not connect to server! -> connect() failed with error code: %d\n", errno);
+                exit(EXIT_FAILURE); // Exit program and return// EXIT_FAILURE (defined as 1 in stdlib.h).
+            } else {
+                printf("(=) Connection with server established.\n\n");
+            }
 
-        // Sending the data and calculating time.
-        int choice;
-        while(true){
-            if(send_data(buffer, sock) == -1){
+            // Sending the data.
+            if (send_data(buffer, sock) == -1) {
                 printf("(-) Failed to send data! -> send() failed with error code: %d\n", errno);
             } else {
                 printf("(+) Sent the data successfully.\n");
             }
-
-            // User decision.
-            printf("Do you want to send file? Enter Y for Yes or N for No.\n"); 
-            while((choice = getchar()) == '\n' || getchar() == EOF);
-            if (choice == 'N' || choice == 'n') {
-                send(sock, "exit", 4, 0); // Send exit message to the server.
-                break;
+            //-------------------------------Close Connection-----------------------------
+            if (close(sock) == -1) {
+                printf("(-) Failed to close connection! -> close() failed with error code: %d\n", errno);
+            } else {
+                printf("(=) Connection closed!\n");
             }
-            printf("-----------------------------------------------------------------\n");
         }
-        printf("(=) Exiting...\n");
-        //-------------------------------Close Connection-----------------------------
-        if(close(sock) == -1) {
-            printf("(-) Failed to close connection! -> close() failed with error code: %d\n", errno);
-        }
-        else {
-            printf("(=) Connection closed!\n");
-        }
-
     }
-
-
-    
-    
-    
 }
 
 //---------------------------------- Server Side - Performance Mode ---------------------------------
 void handle_server_performance (int argc, char* argv[], bool q_flag) {
-// Not the right format.
-    if (argc < 4) {
+    // Not the right format.
+    if (argc > 5) {
         print_server_usage();
         exit(EXIT_FAILURE);
     }
 
     // Setting buffers to hold <type> and <param> from client. Plus 1 for both for /0.
-    char type[5] = {0};
-    char param[7] = {0};
+    char type[TYPE_LEN + 1] = {0};
+    char param[PARAM_LEN + 1] = {0};
 
     // Initialize variables for server.
     struct sockaddr_in serverAddress, clientAddress;
+    unsigned char buffer[BUFFER_SIZE + CHECKSUM_SIZE]; // Buffer to hold data.
+    unsigned char checksum[CHECKSUM_SIZE]; // To hold the server checksum.
 
     // Initialize port.
     int port = atoi(argv[2]);
@@ -489,76 +472,65 @@ void handle_server_performance (int argc, char* argv[], bool q_flag) {
     }
 
     // Receiving <type> and <param> from client.
-    int rbytes1 = recvfrom(socketFD, type, 4, 0, NULL, NULL);
-    int rbytes2 = recvfrom(socketFD, param, 6, 0, NULL, NULL);
+    ssize_t rbytes1 = recvfrom(socketFD, type, TYPE_LEN, 0, NULL, NULL);
+    ssize_t rbytes2 = recvfrom(socketFD, param, PARAM_LEN, 0, NULL, NULL);
     if(rbytes1 == -1 || rbytes2 == -1){
         printf("(-) recv() failed with error code: %d\n", errno);
     }
 
-    // <<<<<<<<<<<<<<<<<<<<<<<<< Handeling All Combinations >>>>>>>>>>>>>>>>>>>>>>>>>
+    // <<<<<<<<<<<<<<<<<<<<<<<<< Handling All Combinations >>>>>>>>>>>>>>>>>>>>>>>>>
+    //  TCP
+    if(!strcmp(param,"tcp")) {
+        // IPv4
+        if (!strcmp(type, "ipv4")) {
+            // Creates TCP socket.
+            int sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    //  IPv4 && TCP
-    if((strcmp(type,"ipv4") == 0) && (strcmp(param,"tcp") == 0)){
-        // Creates TCP socket.
-        int sock = socket(AF_INET, SOCK_STREAM, 0);
+            // Check if address is already in use.
+            int enableReuse = 1;
+            if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enableReuse, sizeof(enableReuse)) == -1) {
+                printf("(-) setsockopt() failed with error code: %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
 
-        // Check if address is already in use.
-        int enableReuse = 1;
-        if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enableReuse, sizeof(enableReuse)) == -1) {
-            printf("(-) setsockopt() failed with error code: %d\n", errno);
-            exit(EXIT_FAILURE);
-        }
-
-        // Initialize variables.
-        struct sockaddr_in serverAddress;
-        memset(&serverAddress, '\0', sizeof(serverAddress));
-
-        // Initialize port.
-        int port = atoi(argv[2]);
-
-        // Assign port and address to "clientAddress".
-        serverAddress.sin_family = AF_INET;
-        serverAddress.sin_port = htons(port); // Short, network byte order.
-        serverAddress.sin_addr.s_addr = INADDR_ANY;
-
-        // Binding port and address to socket and check if binding was successful.
-        if (bind(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) == -1) {
-            printf("(-) Failed to bind address && port to socket! -> bind() failed with error code: %d\n", errno);
-            close(sock);
-            exit(EXIT_FAILURE);
-        } else {
-            printf("(=) Binding was successful!\n");
-        }
-
-        // Make server start listening and waiting, and check if listen() was successful.
-        if (listen(sock, MAX_CONNECTIONS) == -1) { // We allow no more than one queue connections requests.
-            printf("Failed to start listening! -> listen() failed with error code : %d\n", errno);
-            close(sock);
-            exit(EXIT_FAILURE);
-        }
-        while (true) {
-            printf("(=) Waiting for incoming TCP-connections...\n");
-
-            // Create sockaddr_in for IPv4 for holding ip address and port of client and cleans it.
-            struct sockaddr_in clientAddress;
-            memset(&clientAddress, 0, sizeof(clientAddress));
-            unsigned int clientAddressLen = sizeof(clientAddress);
-            // Accept connection.
-            int clientSocket = accept(sock, (struct sockaddr *) &clientAddress, &clientAddressLen);
-            if (clientSocket == -1) {
-                printf("(-) Failed to accept connection. -> accept() failed with error code: %d\n", errno);
+            // Binding port and address to socket and check if binding was successful.
+            if (bind(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) == -1) {
+                printf("(-) Failed to bind address && port to socket! -> bind() failed with error code: %d\n", errno);
                 close(sock);
-                close(clientSocket);
                 exit(EXIT_FAILURE);
             } else {
-                printf("(=) Connection established.\n\n");
+                printf("(=) Binding was successful!\n");
             }
-            // While the user still wants to send file, keep receiving.
-            while(true) {
-                if(recv_data(clientSocket) == -1){
-                    printf("\nExiting...\n\n");
-                    break;
+
+            // Make server start listening and waiting, and check if listen() was successful.
+            if (listen(sock, MAX_CONNECTIONS) == -1) { // We allow no more than one queue connections requests.
+                printf("Failed to start listening! -> listen() failed with error code : %d\n", errno);
+                close(sock);
+                exit(EXIT_FAILURE);
+            }
+            while (true) {
+                printf("(=) Waiting for incoming TCP IPv4-connections...\n");
+
+                // Create sockaddr_in for IPv4 for holding ip address and port of client and cleans it.
+                memset(&clientAddress, 0, sizeof(clientAddress));
+                unsigned int clientAddressLen = sizeof(clientAddress);
+                // Accept connection.
+                int clientSocket = accept(sock, (struct sockaddr *) &clientAddress, &clientAddressLen);
+                if (clientSocket == -1) {
+                    printf("(-) Failed to accept connection. -> accept() failed with error code: %d\n", errno);
+                    close(sock);
+                    close(clientSocket);
+                    exit(EXIT_FAILURE);
+                } else {
+                    printf("(=) Connection established.\n\n");
                 }
+
+                // Receive data from the user.
+                recv_data(clientSocket, type, param, buffer);
+                // Do a checksum in server side.
+                md5_checksum(buffer, BUFFER_SIZE, checksum);
+                // Compare the checksums.
+                check_checksums(checksum, buffer + BUFFER_SIZE);
             }
         }
     }
@@ -590,7 +562,7 @@ unsigned char* generateData (int size) {
         return NULL;
     }
 
-    // Randomaize data.
+    // Randomize data.
     srand(time(NULL));
     for (int i = 0; i < size; i++) {
         result[i] = ((char)rand() % 256);
@@ -604,13 +576,13 @@ unsigned char* generateData (int size) {
 }
 
 //---------------------------------- Compare Checksums ---------------------------------
-bool check_checksums(unsigned char* checksum1, unsigned char* checksum2) {
+void check_checksums(const unsigned char* checksum1, const unsigned char* checksum2) {
     for (int i = 0; i < CHECKSUM_SIZE; i++) {
         if (checksum1[i] != checksum2[i]) {
-            return false;
+            printf("(-) Error: Some data has been lost!");
         }
     }
-    return true;
+    printf("(+) All data was successfully received.");
 }
 
 //---------------------------------- Print Usages ---------------------------------------
@@ -635,24 +607,30 @@ size_t send_data(unsigned char data[], int socketFD) {
 }
 
 //---------------------------------- Receiving Data-----------------------------------------
-int recv_data(int clientSocket) {
-    unsigned char buffer[BUFFER_SIZE+1] = {0};
+void recv_data (int clientSocket, char* type, char* param, unsigned char* buffer) {
+    // Initialize variables.
     size_t receivedTotalBytes = 0; // Variable for keeping track of number of received bytes.
-    while (receivedTotalBytes != BUFFER_SIZE) {
-        bzero(buffer, BUFFER_SIZE + 1); // Clean buffer.
-        ssize_t receivedBytes = recv(clientSocket, buffer, BUFFER_SIZE - receivedTotalBytes, 0);
+    ssize_t receivedBytes = 0;
+
+    // Start measuring time.
+    clock_t start = clock();
+
+    // While there is still data to receive.
+    while ((receivedBytes = recv(clientSocket, buffer, BUFFER_SIZE - receivedTotalBytes, 0)) > 0) {
         if (receivedBytes <= 0) { // Break if we got an error (-1) or peer closed half side of the socket (0).
             printf("(-) Error in receiving data or peer closed half side of the socket.");
             break;
         }
         receivedTotalBytes += receivedBytes; // Add the new received bytes to the total bytes received.
-
-        // Check if we need to exit.
-        if (strcmp(buffer, "exit") == 0) {
-            return -1;
-        }
     }
-    return 0;
+    // Stop measuring time.
+    clock_t end = clock();
+
+    // Calculate time in milliseconds.
+    double duration = (double)(end - start) * 1000.0 / CLOCKS_PER_SEC;
+
+    // Print stats.
+    printf("%s_%s,%f",type, param, duration);
 }
 
     
